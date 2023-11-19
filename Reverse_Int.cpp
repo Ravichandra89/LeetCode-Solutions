4c074761931652371b40dc1f413938f971c536d1
Ravi chandra <134200599+Ravichandra89@users.noreply.github.com>
2023-11-19 21:31:48 +0530

    Create Reverse_Int.cpp

---
+++ Reverse_Int.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while(x!=0){
+            int rem = x%10;
+            // If number is out of integer range than printing zero
+            if((rev>INT_MAX/10) || (rev<INT_MIN/10)){
+                return 0;
+            }
+            rev = (rev*10)+rem;
+            x = x/10;
+        }
+        return rev;
+    }
+};
